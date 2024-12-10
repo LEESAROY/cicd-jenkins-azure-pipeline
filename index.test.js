edcd3307abd9ba11854229d41b4ac368002f9a88
LEESAROY <42925369+LEESAROY@users.noreply.github.com>
2024-12-10 13:46:34 -0500

    made changes

--- index.test.js
+++ index.test.js
@@ -3,16 +3,18 @@ const { createServer } = require('http');
 const handler = require('./index');
 
 function createApp() {
-  return createServer((req, res) => {
-    const context = { res };
-    handler(context, req);
-  });
+    return createServer((req, res) => {
+        const context = { res };
+        handler(context, req);
+    });
 }
 
+jest.setTimeout(10000); // 10 seconds
+
 describe('GET /', () => {
-  it('should return 200 status code and Hello, World! text', async () => {
-    const response = await request(createApp()).get('/');
-    expect(response.statusCode).toBe(200);
-    expect(response.text).toBe('Hello, World!');
-  });
+    it('should return 200 status code and Hello, World! text', async () => {
+        const response = await request(createApp()).get('/');
+        expect(response.statusCode).toBe(200);
+        expect(response.text).toBe('Hello, World!');
+    });
 });
