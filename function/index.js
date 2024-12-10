6e979080cf5a9109a9d929be1b941ac9ade3f9b1
LEESAROY <42925369+LEESAROY@users.noreply.github.com>
2024-12-10 17:59:31 -0500

    update

--- function/index.js
+++ function/index.js
@@ -1,8 +1,14 @@
 module.exports = async function (context, req) {
-    console.log('Handler invoked');
-    context.res.statusCode = 200;
-    context.res.setHeader('Content-Type', 'text/plain');
-    context.res.write("Hello, World!");
-    console.log('Response set');
-    context.res.end();
+    try {
+        console.log('Handler invoked');
+        context.res.statusCode = 200;
+        context.res.setHeader('Content-Type', 'text/plain');
+        context.res.write("Hello, World!");
+        console.log('Response set');
+        context.res.end();
+    } catch (error) {
+        console.error('Error:', error);
+        context.res.statusCode = 500;
+        context.res.end('Internal Server Error');
+    }
 };
